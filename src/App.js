c0a4ddf13448ac3fbe8a45995bd9d03d1cde447a
pk077kumar@gmail.com <pramod.pradhan@giitmail.com>
2023-09-05 20:37:35 +0530

    React project setup and Added for until the useState hook

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Employees from './Employees';
+
+
+function App() {
+  return (
+      <div>
+        <Header/>
+        <Employees />
+        <Footer />
+      </div>
+
+     
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+
+    return (
+        <footer>
+            <h1> Footer section </h1>
+        </footer>
+    )
+}
+
+export default Footer 
